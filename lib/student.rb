cd3e86ccaaf3a339cd079836ce7c463a0906744c
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 04:33:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,15 @@ class Student
     end.first
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+      LIMIT 2
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_student_in_grade_10
