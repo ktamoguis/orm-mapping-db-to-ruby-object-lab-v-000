5dac5b1a0b792307732ff0e227fae58f107ba565
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 04:44:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,7 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      GROUP BY grade
-      HAVING grade < 12
+      WHERE grade < 12
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
