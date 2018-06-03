f4d2f6b9b3282abf94b09059ffd3d84a81e58794
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 04:44:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
       GROUP BY grade
       HAVING grade < 12
     SQL
-    DB[:conn].execute(sql, x).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
   end
