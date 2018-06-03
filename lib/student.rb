8735d0a0add0c839b9d9b948b777105175a64710
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 04:28:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,6 +35,15 @@ class Student
     end.first
   end
 
+  def self.first_X_students_in_grade_10
+  end
+
+  def self..first_student_in_grade_10
+  end
+
+  def self.all_students_in_grade_X
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
